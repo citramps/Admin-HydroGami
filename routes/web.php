cb4ad4d1f7766454ca5ebea81163cd2e77900707
naniaprima <159672664+cabbageflower@users.noreply.github.com>
2025-07-05 00:02:14 +0700

    ganti web.php

--- routes/web.php
+++ routes/web.php
@@ -22,55 +22,39 @@ Route::get('/login', function () {
     return redirect()->route('login-admin');
 })->name('login');
 
-Route::post('/logout', [AuthController::class, 'logoutAdmin'])->name('logout');
-
-// Halaman dashboard
-Route::get('dashboard-admin', [DashboardController::class, 'index'])
+// UBAH: Tambah '/' dan ganti middleware jadi 'auth:admin'
+Route::get('/dashboard-admin', [DashboardController::class, 'index'])
     ->name('dashboard-admin')
-    ->middleware('auth');
+    ->middleware('auth:admin');
 
-
-// Halaman misi
-Route::middleware('auth')->group(function () {
+// UBAH: Ganti semua middleware jadi 'auth:admin'
+Route::middleware('auth:admin')->group(function () {
+    // Halaman misi
     Route::get('/misi', [MisiController::class, 'index'])->name('misi.index');
     Route::get('/misi/create', [MisiController::class, 'create'])->name('misi.create');
     Route::post('/misi', [MisiController::class, 'store'])->name('misi.store');
     Route::get('/misi/{id_misi}/edit', [MisiController::class, 'edit'])->name('misi.edit');
     Route::put('/misi/{id_misi}', [MisiController::class, 'update'])->name('misi.update');
     Route::delete('/misi/{id_misi}', [MisiController::class, 'destroy'])->name('misi.destroy');
-});
 
-
-// Halaman panduan
-Route::middleware('auth')->group(function () {
+    // Halaman panduan
     Route::get('/panduan', [PanduanController::class, 'index'])->name('panduan.index');
     Route::get('/panduan/create', [PanduanController::class, 'create'])->name('panduan.create');
     Route::post('/panduan', [PanduanController::class, 'store'])->name('panduan.store');
     Route::get('/panduan/{id_panduan}/edit', [PanduanController::class, 'edit'])->name('panduan.edit');
     Route::put('/panduan/{id_panduan}', [PanduanController::class, 'update'])->name('panduan.update');
     Route::delete('/panduan/{id_panduan}', [PanduanController::class, 'destroy'])->name('panduan.destroy');
-});
 
+    // Halaman leaderboard
+    Route::get('/leaderboard-admin', [LeaderboardController::class, 'index'])->name('leaderboard-admin');
 
-// Halaman leaderboard
-Route::get('/leaderboard-admin', [LeaderboardController::class, 'index'])
-    ->name('leaderboard-admin')
-    ->middleware('auth');
-
-
-// Halaman profile
-Route::middleware(['auth'])->group(function () {
-    Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
-    Route::get('profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Halaman profile
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
 
-
-});
-
-// Halaman Rewards
-
-Route::middleware('auth')->group(function () {
+    // Halaman Rewards
     Route::get('/reward', [RewardController::class, 'index'])->name('reward.index');
     Route::get('/reward/create', [RewardController::class, 'create'])->name('reward.create');
     Route::post('/reward', [RewardController::class, 'store'])->name('reward.store');
@@ -79,5 +63,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/reward/{id}', [RewardController::class, 'destroy'])->name('reward.destroy');
 });
 
-// Logout 
-Route::post('logout', [AuthController::class, 'logout'])->name(name: 'logout');
+// UBAH: Satu route logout aja, pakai guard admin
+Route::post('/logout', [AuthController::class, 'logoutAdmin'])->name('logout');
