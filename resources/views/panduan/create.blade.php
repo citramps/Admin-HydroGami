e91fc78a93d1edc53ac60096af016792def98a31
naniaprima <159672664+cabbageflower@users.noreply.github.com>
2025-07-20 15:58:15 +0700

    perbaiki nama halaman

--- resources/views/panduan/create.blade.php
+++ resources/views/panduan/create.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Misi Gamifikasi</title>
+    <title>Tambah Panduan</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet">
